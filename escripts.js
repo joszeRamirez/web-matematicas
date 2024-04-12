81693f40dbcc2da4fb5b1ab3247df48066913267
joszeRamirez <jramirezc9@est.ups.edu.ec>
2024-04-12 01:44:40 -0500

    Arreglo de calculadora y adición de cambio de secciones

--- escripts.js
+++ escripts.js
@@ -1,216 +1,250 @@
 
-  var toperador ="";
-  const tcoma = '.';
-  const tnega = '-';
-  const pantalla = "";
-  var almacenaje = 0;
-  var operacion = 0;
+var almacenaje = 0;
+var operacion = 0;
+var toperador = "";
+const tnega = '-';
 
- function limitar() {
+function limitar() {
     let ver = false;
     if (document.getElementById("vistazo").value.length <= 9) {
-      ver = true;
+        ver = true;
     }
     return ver;
-  }
+}
 
-  function borra1() {
-    this.pantalla = this.pantalla.substring(0, this.pantalla.length - 1)
-  }
+function borra1() {
+    let pantalla = "";
+    pantalla = document.getElementById('vistazo').value;
+    pantalla = pantalla.substring(0, pantalla.length - 1);
+    document.getElementById('vistazo').value = pantalla;
+}
 
-  function borrabajo() {
-    this.pantalla = ""
-  }
+function borrabajo() {
+    document.getElementById('vistazo').value = "";
+}
 
-  function borratodo() {
-    this.pantalla = ""
-    this.almacenaje = 0
-    this.toperador = ''
-  }
+function borratodo() {
+    document.getElementById('vistazo').value = "";
+    document.getElementById('almacenaje').value = "";
+    toperador = '';
+}
 
-  function teklar1() {
+function teklar1() {
     if (limitar()) {
-      document.getElementById('vistazo').value += document.getElementById("1").innerHTML;
-        // pantalla += t1;
-      
-      console.log("si vale")
+        document.getElementById('vistazo').value += document.getElementById("1").innerHTML;
+
     } else {
-      alert("Excede el límite de números")
+        alert("Excede el límite de números");
     }
-  }
-  function teklar2() {
+}
+function teklar2() {
     if (this.limitar()) {
         document.getElementById('vistazo').value += document.getElementById("2").innerHTML;
-      console.log(this.pantalla)
+
     } else {
-      alert("Excede el límite de números")
+        alert("Excede el límite de números");
     }
-  }
-  function teklar3() {
+}
+function teklar3() {
     if (this.limitar()) {
         document.getElementById('vistazo').value += document.getElementById("3").innerHTML;
-      console.log(this.pantalla)
+
     } else {
-      alert("Excede el límite de números")
+        alert("Excede el límite de números");
     }
-  }
-  function teklar4() {
+}
+function teklar4() {
     if (this.limitar()) {
         document.getElementById('vistazo').value += document.getElementById("4").innerHTML;
-      console.log(this.pantalla)
+
     } else {
-      alert("Excede el límite de números")
+        alert("Excede el límite de números");
     }
-  }
-  function teklar5() {
+}
+function teklar5() {
     if (this.limitar()) {
         document.getElementById('vistazo').value += document.getElementById("5").innerHTML;
-      console.log(this.pantalla)
+
     } else {
-      alert("Excede el límite de números")
+        alert("Excede el límite de números");
     }
-  }
-  function teklar6() {
+}
+function teklar6() {
     if (this.limitar()) {
         document.getElementById('vistazo').value += document.getElementById("6").innerHTML;
-      console.log(this.pantalla)
+
     } else {
-      alert("Excede el límite de números")
+        alert("Excede el límite de números");
     }
-  }
-  function teklar7() {
+}
+function teklar7() {
     if (this.limitar()) {
         document.getElementById('vistazo').value += document.getElementById("7").innerHTML;
-      console.log(this.pantalla)
+
     } else {
-      alert("Excede el límite de números")
+        alert("Excede el límite de números");
     }
-  }
-  function teklar8() {
+}
+function teklar8() {
     if (this.limitar()) {
         document.getElementById('vistazo').value += document.getElementById("8").innerHTML;
-      console.log(this.pantalla)
+
     } else {
-      alert("Excede el límite de números")
+        alert("Excede el límite de números");
     }
-  }
-  function teklar9() {
+}
+function teklar9() {
     if (this.limitar()) {
         document.getElementById('vistazo').value += document.getElementById("9").innerHTML;
-      console.log(this.pantalla)
+
     } else {
-      alert("Excede el límite de números")
+        alert("Excede el límite de números");
     }
-  }
-  function teklar0() {
+}
+function teklar0() {
     if (this.limitar()) {
         document.getElementById('vistazo').value += document.getElementById("0").innerHTML;
-      console.log(this.pantalla)
-    } else {
-      alert("Excede el límite de números")
-    }
-  }
 
-  function teklarComa() {
+    } else {
+        alert("Excede el límite de números");
+    }
+}
+
+function teklarComa() {
     if (this.limitar()) {
-      if (this.pantalla.includes('.')) {
-        alert("Ya existe 1 decimal")
-      } else {
-        document.getElementById('vistazo').value += document.getElementById(",").innerHTML;
-        console.log(this.pantalla)
-      }
+        let pantalla = document.getElementById('vistazo').value;
+        if (pantalla.includes(',')) {
+            alert("Ya existe 1 decimal");
+        } else {
+            document.getElementById('vistazo').value += document.getElementById("bcoma").innerHTML;
+        }
     } else {
-      alert("Excede el límite de números")
+        alert("Excede el límite de números")
     }
-  }
+}
 
-  function teklarMenos() {
+function teklarMenos() {
     if (this.limitar()) {
-      let coma = this.pantalla.charAt(0);
-      if (coma == '-') {
-        this.pantalla = this.pantalla.substring(1, this.pantalla.length)
-        console.log(this.pantalla)
-      } else {
-        this.pantalla = this.tnega + this.pantalla;
-        console.log(this.pantalla)
-      }
+        let pantalla = document.getElementById('vistazo').value;
+        let coma = pantalla.charAt(0);
+        if (coma == '-') {
+            pantalla = pantalla.substring(1, pantalla.length);
+            document.getElementById('vistazo').value = pantalla;
+            console.log(pantalla)
+        } else {
+            document.getElementById('vistazo').value = tnega + pantalla;
+            console.log(pantalla)
+        }
 
     } else {
-      alert("Excede el límite de números")
+        alert("Excede el límite de números")
     }
-  }
+}
 
-  function sumar() {
-    this.operacion = 1
-    if (this.almacenaje != 0) {
-      this.almacenaje = this.almacenaje + Number(this.pantalla)
-      if (this.limitar()) {
-        this.pantalla = this.almacenaje.toString()
-      }
+function sumar() {
+    operacion = 1
+    almacenaje = parseFloat(document.getElementById('almacenaje').value);
+    console.log("valor almacenado " + almacenaje + typeof almacenaje);
+    let pantalla = parseFloat(document.getElementById('vistazo').value);
+    console.log("valor pantalla " + pantalla + typeof pantalla)
+    if (!isNaN(almacenaje)) {
+        almacenaje = almacenaje + pantalla;
+        console.log("valor almacenado " + almacenaje + typeof almacenaje);
+        if (this.limitar()) {
+            document.getElementById('vistazo').value = almacenaje;
+            document.getElementById('almacenaje').value = pantalla;
+        }
     } else {
-      this.almacenaje = Number(this.pantalla)
-      this.pantalla = ""
+        document.getElementById('almacenaje').value = pantalla;
+        document.getElementById('vistazo').value = "";
     }
-    this.toperador = "+"
-  }
-  function restar() {
-    this.operacion = 2
-    if (this.almacenaje != 0) {
-      this.almacenaje = this.almacenaje - Number(this.pantalla)
-      if (this.limitar()) {
-        this.pantalla = this.almacenaje.toString()
-      }
+    toperador = "+"
+}
+function restar() {
+    operacion = 2
+    almacenaje = parseFloat(document.getElementById('almacenaje').value);
+    let pantalla = parseFloat(document.getElementById('vistazo').value);
+    if (!isNaN(almacenaje)) {
+        almacenaje = almacenaje - pantalla;
+        console.log("valor almacenado " + almacenaje + typeof almacenaje);
+        if (this.limitar()) {
+            document.getElementById('vistazo').value = almacenaje;
+            document.getElementById('almacenaje').value = pantalla;
+        }
     } else {
-      this.almacenaje = Number(this.pantalla)
-      this.pantalla = ""
+        document.getElementById('almacenaje').value = pantalla;
+        document.getElementById('vistazo').value = "";
     }
-    this.toperador = "-"
-  }
-  function multiplicar() {
-    this.operacion = 3
-    if (this.almacenaje != 0) {
-      this.almacenaje = this.almacenaje * Number(this.pantalla)
-      if (this.limitar()) {
-        this.pantalla = this.almacenaje.toString()
-      }
+    toperador = "-"
+}
+function multiplicar() {
+    operacion = 3
+    almacenaje = parseFloat(document.getElementById('almacenaje').value);
+    let pantalla = parseFloat(document.getElementById('vistazo').value);
+    if (!isNaN(almacenaje)) {
+        almacenaje = almacenaje * pantalla;
+        console.log("valor almacenado " + almacenaje + typeof almacenaje);
+        if (this.limitar()) {
+            document.getElementById('vistazo').value = almacenaje;
+            document.getElementById('almacenaje').value = pantalla;
+        }
     } else {
-      this.almacenaje = Number(this.pantalla)
-      this.pantalla = ""
+        document.getElementById('almacenaje').value = pantalla;
+        document.getElementById('vistazo').value = "";
     }
-    this.toperador = "x"
-  }
-  function dividir() {
-    this.operacion = 4
-    if (this.almacenaje != 0) {
-      this.almacenaje = this.almacenaje / Number(this.pantalla)
-      if (this.limitar()) {
-        this.pantalla = this.almacenaje.toString()
-      }
+    toperador = "x"
+}
+function dividir() {
+    operacion = 4
+    almacenaje = parseFloat(document.getElementById('almacenaje').value);
+    let pantalla = parseFloat(document.getElementById('vistazo').value);
+    if (!isNaN(almacenaje)) {
+        almacenaje = almacenaje / pantalla;
+        console.log("valor almacenado " + almacenaje + typeof almacenaje);
+        if (this.limitar()) {
+            document.getElementById('vistazo').value = almacenaje;
+            document.getElementById('almacenaje').value = pantalla;
+        }
     } else {
-      this.almacenaje = Number(this.pantalla)
-      this.pantalla = ""
+        document.getElementById('almacenaje').value = pantalla;
+        document.getElementById('vistazo').value = "";
     }
-    this.toperador = "/"
-  }
-  function resultar() {
-    switch (this.operacion) {
-      case 1:
-        this.sumar()
-        break;
-      case 2:
-        this.restar()
-        break;
-      case 3:
-        this.multiplicar()
-        break;
-      case 4:
-        this.dividir()
-        break;
-      default:
-        alert("No hay operador")
-        break;
+    toperador = "/"
+}
+function resultar() {
+    switch (operacion) {
+        case 1:
+            sumar()
+            break;
+        case 2:
+            restar()
+            break;
+        case 3:
+            multiplicar()
+            break;
+        case 4:
+            dividir()
+            break;
+        default:
+            alert("No hay operador")
+            break;
     }
-    this.operacion = 0
-    this.pantalla = this.almacenaje.toString()
-    this.toperador = '';
-  }
+    operacion = 0;
+    pantalla = this.almacenaje.toString();
+    toperador = '';
+}
+
+function mostrarOperaciones() {
+    const calcu = document.querySelector(".contecalcu");
+    const opera = document.querySelector(".operaciones");
+    calcu.setAttribute("hidden", "true");
+    opera.removeAttribute("hidden");
+}
+
+function mostrarCalcu() {
+    const calcu = document.querySelector(".contecalcu");
+    const opera = document.querySelector(".operaciones");
+    opera.setAttribute("hidden", "true");
+    calcu.removeAttribute("hidden");
+
+}
